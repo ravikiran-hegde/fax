1be96c5f4f973f34137e3eeb467c3be7f53f0c13
ravikiran-hegde <ravikiranhegde01@gmail.com>
2026-06-28 14:22:54 +0200

    step towards module

---
+++ model/__init__.py
@@ -0,0 +1,25 @@
+from . import (
+    abstract_class,
+    arts,
+    constants,
+    continuum,
+    forms,
+    functional,
+    gas_optics,
+    single_absorber,
+    utils,
+    xfit,
+)
+
+__all__ = [
+    "abstract_class",
+    "constants",
+    "forms",
+    "gas_optics",
+    "utils",
+    "arts",
+    "continuum",
+    "functional",
+    "single_absorber",
+    "xfit",
+]
